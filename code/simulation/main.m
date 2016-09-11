5e9b7324a26de0993ccb84078f47e6fa5dd0f308
Akshit09 <akshitsinghal96@gmail.com>
2016-09-11 16:24:18 +0530

    Update main.m

--- code/simulation/main.m
+++ code/simulation/main.m
@@ -3,7 +3,7 @@ HammingDistance = zeros(406, 13168);    %Declaration of hamming Distance Matrix
 CosineSimilarity = zeros(406, 13168);   %Declaration of Cosine Similarity Matrix
 randomNum=randperm(11142,410);          % 1D matrix of Random Number ranging from 1 to 11142
 
-for imageNumber = 6:6                   % Loop for 400 images
+for imageNumber = 406:6                   % Loop for 400 images
     columnNumber=2;                     %Column
     path=ImagPath(randomNum(imageNumber));
     % imagPath function to get the path of the image
